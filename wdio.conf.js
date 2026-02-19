81cd8bd01a47614309cb86e8775c9c46f614ed54
EvansLe <lewis.evans@ros.gov.uk>
2026-02-19 10:51:28 +0000

    updated again

--- wdio.conf.js
+++ wdio.conf.js
@@ -50,11 +50,12 @@ exports.config = {
     // Sauce Labs platform configurator - a great tool to configure your capabilities:
     // https://saucelabs.com/platform/platform-configurator
     //
-    capabilities: [{
-        browserName: 'chrome',
+capabilities: [{
+    browserName: 'chrome',
     'goog:chromeOptions': {
         args: ['headless', 'disable-gpu']
-    }],
+    }
+}],
 
     //
     // ===================
