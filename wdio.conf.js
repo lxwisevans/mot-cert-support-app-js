2a305a90529e4a6dd5711f6fe13bfe44a81f786e
EvansLe <lewis.evans@ros.gov.uk>
2026-02-19 12:40:02 +0000

    updated again

--- wdio.conf.js
+++ wdio.conf.js
@@ -127,15 +127,13 @@ capabilities: [{
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter
-    
+
 reporters: [
   ['junit', {
-    outputDir: './reports/e2e-report/',
-    outputFileFormat: function(options) {
-      return `results.xml`
-    }
+    outputDir: './reports/e2e/',
+    outputFileFormat: () => 'junit-e2e.xml'
   }]
-]
+],
 
 
     // Options to be passed to Mocha.
