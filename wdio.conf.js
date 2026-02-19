4383a36fff38c40d463e40c1ab796e0f97cec47e
EvansLe <lewis.evans@ros.gov.uk>
2026-02-19 12:30:02 +0000

    Added reporting to Github Actions

--- wdio.conf.js
+++ wdio.conf.js
@@ -127,7 +127,12 @@ capabilities: [{
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter
-    reporters: ['spec','junit'],
+  reporters: [['junit',{
+    outputDir: './reports/e2e-report/',
+    outputFileFormat: function(options) {
+        return 'junit-report.xml'
+    }
+}]],
 
     // Options to be passed to Mocha.
     // See the full list at http://mochajs.org/
