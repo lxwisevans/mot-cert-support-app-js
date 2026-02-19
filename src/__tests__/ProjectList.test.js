f274a4013a1ac5539772ef0adb627584387c3300
EvansLe <lewis.evans@ros.gov.uk>
2026-02-19 10:13:19 +0000

    Addded in Github actions

---
+++ src/__tests__/ProjectList.test.js
@@ -0,0 +1,29 @@
+import ProjectList from '../components/ProjectList';
+import { MemoryRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import nock from 'nock';
+
+// Step 2.2: Mock API
+nock('http://localhost')
+    .persist()
+    .get('/v1/project')
+    .reply(200, [
+        { id: '1', name: 'Project 1', description: 'This is the first project' },
+        { id: '2', name: 'Project 2', description: 'This is the second project' }
+    ]);
+
+// Step 2.3: Unit test
+describe('ProjectList', () => {
+    it('renders the list of projects correctly', async () => {
+        const { asFragment, findByText } = render(
+            <MemoryRouter>
+                <ProjectList />
+            </MemoryRouter>
+        );
+
+        await findByText('Project 1');
+
+        expect(asFragment()).toMatchSnapshot();
+    });
+});
